b84b834307d72c70ef86f8cfd5bb76988c04a0fc
Maksym Khimii <meizovicmaks@gmail.com>
2023-01-23 23:01:05 +0200

    post deletion has been done

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -11,6 +11,7 @@ import {Subscription} from "rxjs";
 export class DashboardPageComponent implements OnInit, OnDestroy {
   posts: Post[] = []
   pSub: Subscription
+  dSub: Subscription
   searchStr: string = ''
 
   constructor(private postsService: PostsService) {
@@ -22,13 +23,18 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     })
   }
 
+  remove(id: string) {
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id)
+    })
+  }
+
   ngOnDestroy(): void {
     if (this.pSub) {
       this.pSub.unsubscribe()
     }
-  }
-
-  remove(id: string) {
-
+    if (this.dSub) {
+      this.dSub.unsubscribe()
+    }
   }
 }
