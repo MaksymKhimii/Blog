a972245a1af5595fef3b90b74e1822f742b1af9c
Maksym Khimii <meizovicmaks@gmail.com>
2023-01-23 18:40:13 +0200

    new interceptors and post creation and save in firebase with auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyAK0TJurgixmQ3Eudp_YTk1K_ohAkJ-4yo'
+  apiKey: 'AIzaSyAK0TJurgixmQ3Eudp_YTk1K_ohAkJ-4yo',
+  fbDbUrl: 'https://blog-c718a-default-rtdb.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyAK0TJurgixmQ3Eudp_YTk1K_ohAkJ-4yo'
+  apiKey: 'AIzaSyAK0TJurgixmQ3Eudp_YTk1K_ohAkJ-4yo',
+  fbDbUrl: 'https://blog-c718a-default-rtdb.firebaseio.com'
 };
 
 /*
